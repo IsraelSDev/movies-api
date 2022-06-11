af0af9e89d5b2aa98b9e201b7af193960d204eb2
Israel S. Santos <israel.santos@zukk.com.br>
2022-06-11 11:05:04 -0300

    fix: cors blocked added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 
 const movies = require("./src/movies/movies.json");
+
+app.use(cors());
 
 app.get("/movies", (req, res) => {
   return res.json(movies);
