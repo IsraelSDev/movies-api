e96bc04bce2eed1b0b80210adc27a132685c9680
Israel S. Santos <israel.santos@zukk.com.br>
2022-06-11 09:49:03 -0300

    rm: fix api index

--- index.js
+++ index.js
@@ -4,18 +4,8 @@ const port = process.env.PORT || 3000;
 
 const movies = require("./src/movies/movies.json");
 
-const moviesFiltered = (name) => {
-  movies.filter((movie) => {
-    return movie.name.toLowerCase().includes(name.toLowerCase());
-  });
-};
-
 app.get("/movies", (req, res) => {
   return res.json(movies);
-});
-
-app.get("/movies/:name", (req, res) => {
-  return res.json(moviesFiltered(req.params.name));
 });
 
 app.listen(port, () => {
